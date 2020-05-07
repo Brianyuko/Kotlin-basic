a545ff053baf93a01a07859e01e6452466e8701e
Brianyuko <brianyuko45@gmail.com>
2020-05-08 06:58:15 +0700

    var dan val

---
+++ VarAndVal.kt
@@ -0,0 +1,9 @@
+fun main(){
+
+    val tinggiBadan = 150 // Val object Immutable atau tidak dapat diubah
+    // tinggiBadan = 120 Baris ini akan error, Val Cannot be Reassigned
+    var nama = "Brian Yuko" // Var object Mutable atau dapat diubah
+    nama = "Brian Yuko Putra" // Ini bisa dijalankan karena var dapat diubah
+    println("Nama saya adalah $nama, memiliki tinggi badan $tinggiBadan")
+
+}
