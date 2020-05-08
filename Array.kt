c63511db05422ee10afa467d480428cf0950c8c0
Brianyuko <brianyuko45@gmail.com>
2020-05-09 06:04:06 +0700

    Add Method Array

--- Array.kt
+++ Array.kt
@@ -2,6 +2,7 @@ fun main(){
 
     // Tipe Data Array
     // Sifat dari array adalah mutable atau bisa diubah
+
     val angka = arrayOf<Int>(1,2,3,4,5) // <Kumpulan tipe data>
     val absen = arrayOf("Angga", "Arysi", "Aje")
     println(absen[0]) // Akan mengagambil index 0 yaitu Angga
@@ -11,4 +12,12 @@ fun main(){
     println(absen[0])
     println(absen.toList()) // Data akan terupdate sesuai dengan aray yang diganti
     println(angka.toList())
+
+    // Beberapa Method Array
+    println("Last dari array absen ${absen.last()}") // Last in Array
+    println("First dari array absen ${absen.first()}") // First in Array
+    println("Rata rata dari array angka ${angka.average()}") // Average in Array
+    println("Nilai maximal dari array angka ${angka.max()}") // Value max in Array
+    println("Nilai minimal dari array angka ${angka.min()}") // Value min in Array
+    println("Isi array ada ${absen.count()}") // Count content in Array
 }
