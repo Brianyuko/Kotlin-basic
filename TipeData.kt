b4fbd2b0a475a62708ba523a0e405b02fe40999b
Brianyuko <brianyuko45@gmail.com>
2020-05-08 07:37:42 +0700

    Tipe data Kotlin

---
+++ TipeData.kt
@@ -0,0 +1,18 @@
+fun main(){
+
+    // Tipe data diawali dengan huruf kapital
+    val umur: Int = 30
+    val nama: String = "Arsyi" // String, tipe data teks
+    val lulus: Boolean = true // Boolean, tipe data yang hanya bernilai true atau false
+    val nilaiMatematika: Float = 21.23F // Float, tipe data pecahan
+    val nilaiNonAkademik: Double = 98.123123 // Double, tipe data pecahan, ukuran penyimpanan lebih besar dari float
+    val nilaiFotografi: Char = 'A' // Char, tipe data karakter
+
+    println("Nama Mahasiswa : $nama")
+    println("Umur Mahasiswa : $umur")
+    println("Nilai Matematika : $nilaiMatematika")
+    println("Nilai Non Akademik: $nilaiFotografi atau $nilaiNonAkademik")
+    print("Lulus bernilai $lulus")
+
+
+}
